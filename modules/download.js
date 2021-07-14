8a144fb4cda2b59f037ca3afe5d3cbef9f47f738
cucearda <cuce.arda99@gmail.com>
2021-07-14 11:51:36 +0300

    conn prop. deleted

--- modules/download.js
+++ modules/download.js
@@ -1,13 +1,6 @@
 var Client = require('ftp');
 var fs = require('fs');
 
-var connectionProperties = {
-  host: "waws-prod-db3-083.ftp.azurewebsites.windows.net",
-  user: "pl-website",
-  password: "55hweHHteAceqs29qtnoWQLXyBwvggzGol9JN8aMoggcBGHbLavCJMynXD3M",
-  protocol: "ftps"
-};
-
 var c = new Client();
 c.on('ready', function () {
   c.get('./site/test/test.zip', function (err, stream) {
